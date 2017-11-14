9bfb82d91a47b4f7187aa1b21f82ff9c7eef6cde
Andrew Khalaf <andy.ehab@aucegypt.edu>
2017-11-14 15:13:42 +0200

    qselect

--- Problem 1/problem 1/Source.cpp
+++ Problem 1/problem 1/Source.cpp
@@ -10,3 +10,44 @@ void main()
 
 
 }
+
+
+int qselect(int A[], int p, int r, int k)
+{
+	if (p == r) return A[p];
+	else
+	{
+
+		int q = partition(A, p, r + 1);
+		int L = q - p + 1;
+		if (k == L) return A[q];
+		else
+			if (k < L) return qselect(A, p, q - 1, k);
+			else return qselect(A, q + 1, r, k - L);
+	}
+
+
+
+
+}
+
+int partition(int A[], int p, int r)
+{
+	int pivot = A[p];
+	int i = p, j = r;
+
+	do {
+		do { i++; } while (A[i] < pivot);
+		do { j--; } while (A[j] > pivot);
+
+		if (i < j) swap(A[i], A[j]);
+	
+	} while (i < j);
+
+	A[p] = A[j];
+	A[j] = pivot; 
+
+	return j;
+
+
+}
